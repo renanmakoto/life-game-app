8189ccd3026a5b26cbf88abcf0639e0ef1b6dd1a
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2023-02-25 17:16:44 -0300

    Add files via upload
    Notification system was added.

---
+++ src/Services/NotificationService.js
@@ -0,0 +1,59 @@
+import * as Notifications from "expo-notifications"
+
+async function createNotification(
+    habitInput,
+    frequencyInput,
+    dayNotification,
+    timeNotification
+) {
+    const habitHour = Number(timeNotification.slice(0, 2))
+    const habitMinutes = Number(timeNotification.slice(3, 5))
+    
+    let weekDay
+    
+    if (dayNotification === "Monday") {
+       weekDay = 1
+    } else if (dayNotification === "Tuesday") {
+       weekDay = 2
+    } else if (dayNotification === "Wednesday") {
+       weekDay = 3
+    } else if (dayNotification === "Thursday") {
+       weekDay = 4
+    } else if (dayNotification === "Friday") {
+       weekDay = 5
+    } else if (dayNotification === "Saturday") {
+       weekDay = 6
+    } else if (dayNotification === "Sunday") {
+       weekDay = 7
+    }
+}
+
+let triggerNotification
+
+if (frequencyInput === "Daily") {
+    triggerNotification = {
+        hour: habitHour,
+        minute: habitMinutes,
+        repeats: true,
+    }
+} else if (frequencyInput === "Weekly") {
+    triggerNotification = {
+        hour: habitHour,
+        minute: habitMinutes,
+        repeats: true,
+        weekday: weekDay,
+    }
+}
+
+await Notifications.scheduleNotificationAsync({
+    content: {
+        title: "Habit reminder",
+        body: `${habitInput}`,
+    },
+    identifier: `${habitInput}`,
+    trigger: triggerNotification,
+}).then((id) => {
+    console.log(id)
+})
+
+export default { createNotification }
--- src/pages/HabitPage/HabitPage.jsx
+++ src/pages/HabitPage/HabitPage.jsx
@@ -9,6 +9,7 @@ import {
     Alert 
 } from "react-native"
 import { useNavigation } from "@react-navigation/native"
+import * as Notifications from "expo-notifications"
 
 import SelectHabit from "../../Components/HabitPage/SelectHabit"
 import SelectFrequency from "../../Components/HabitPage/SelectFrequency"
@@ -17,6 +18,14 @@ import TimeDataPicker from "../../Components/HabitPage/TimeDataPicker"
 import UpdateDeleteButton from "../../Components/HabitPage/UpdateDeleteButtons"
 import DefaultButton from "../../Components/Common/DefaultButton/DefaultButton"
 import HabitsService from "../../Services/HabitsService"
+
+Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: false,
+      shouldSetBadge: false,
+    }),
+})
 
 export default function HabitPage({ route }) {
     const navigation = useNavigation()
@@ -29,7 +38,11 @@ export default function HabitPage({ route }) {
     const { create, habit } = route.params
 
     const habitCreated = new Date()
-    const formatDate = `${habitCreated.getFullYear()}-${habitCreated.getMonth()}-${habitCreated.getDate()}`
+    const formatDate = `${habitCreated.getFullYear()}-${habitCreated.getMonth() + 1}-${habitCreated.getDate()}`
+
+    const [notification, setNotification] = useState(false)
+    const notificationListener = useRef()
+    const responseListener = useRef()
 
     function handleCreateHabit() {
         if (habitInput === undefined || frequencyInput === undefined) {
@@ -91,11 +104,40 @@ export default function HabitPage({ route }) {
 
                 }
             })
+
             navigation.navigate("Home", {
                 updatedHabit: `Updated in ${habit?.habitArea}`,
             })
         }
     }
+
+    useEffect(() => {
+        if (habit?.habitHasNotification == 1) {
+            setNotificationToggle(true)
+            setDayNotification(habit?.habitNotificationFrequency)
+            setTimeNotification(habit?.habitNotificationTime)
+        }
+    }, [])
+
+    useEffect(() => {
+        if (notificationToggle === false) {
+            setTimeNotification(null)
+            setDayNotification(null)
+        }
+    }, [notificationToggle])
+
+    useEffect(() => {
+        notificationListener.current = Notifications.addNotificationReceivedListener((notification) => {
+            setNotification(notification)
+        })
+        responseListener.current = Notifications.addNotificationResponseReceivedListener((response) => {
+            console.log(response)
+        })
+        return () => {
+            Notifications.removeNotificationSubscription(notificationListener.current)
+            Notifications.removeNotificationSubscription(responseListener.current)
+        }
+    }, [])
 
     return (
         <View style={styles.container}>
