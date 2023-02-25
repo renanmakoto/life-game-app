cc3a443f5b5fecaaa5fa4d82129a98a1a21705d7
Renan Makoto <65373130+renanmakoto@users.noreply.github.com>
2023-02-25 17:37:40 -0300

    Add files via upload
    Delete notification was added.

--- src/Services/NotificationService.js
+++ src/Services/NotificationService.js
@@ -1,60 +1,69 @@
 import * as Notifications from "expo-notifications"
 
 async function createNotification(
-    habitInput,
-    frequencyInput,
-    dayNotification,
-    timeNotification
+  habitInput,
+  frequencyInput,
+  dayNotification,
+  timeNotification
 ) {
-    const habitHour = Number(timeNotification.slice(0, 2))
-    const habitMinutes = Number(timeNotification.slice(3, 5))
-    
-    let weekDay
-    
-    if (dayNotification === "Monday") {
-       weekDay = 1
-    } else if (dayNotification === "Tuesday") {
-       weekDay = 2
-    } else if (dayNotification === "Wednesday") {
-       weekDay = 3
-    } else if (dayNotification === "Thursday") {
-       weekDay = 4
-    } else if (dayNotification === "Friday") {
-       weekDay = 5
-    } else if (dayNotification === "Saturday") {
-       weekDay = 6
-    } else if (dayNotification === "Sunday") {
-       weekDay = 7
-    }
+  const habitHour = Number(timeNotification.slice(0, 2))
+  const habitMinutes = Number(timeNotification.slice(3, 5))
 
+  let weekDay
 
-let triggerNotification
+  if (dayNotification === "Monday") {
+    weekDay = 1
+  } else if (dayNotification === "Tuesday") {
+    weekDay = 2
+  } else if (dayNotification === "Wednesday") {
+    weekDay = 3
+  } else if (dayNotification === "Thursday") {
+    weekDay = 4
+  } else if (dayNotification === "Friday") {
+    weekDay = 5
+  } else if (dayNotification === "Saturday") {
+    weekDay = 6
+  } else if (dayNotification === "Sunday") {
+    weekDay = 7
+  }
 
-if (frequencyInput === "Daily") {
+  let triggerNotification
+
+  if (frequencyInput === "Daily") {
     triggerNotification = {
-        hour: habitHour,
-        minute: habitMinutes,
-        repeats: true,
+      hour: habitHour,
+      minute: habitMinutes,
+      repeats: true,
     }
-} else if (frequencyInput === "Weekly") {
+  } else if (frequencyInput === "Weekly") {
     triggerNotification = {
-        hour: habitHour,
-        minute: habitMinutes,
-        repeats: true,
-        weekday: weekDay,
+      hour: habitHour,
+      minute: habitMinutes,
+      repeats: true,
+      weekday: weekDay,
     }
-}
+  }
 
-await Notifications.scheduleNotificationAsync({
+  await Notifications.scheduleNotificationAsync({
     content: {
-        title: "Habit reminder",
-        body: `${habitInput}`,
+      title: "Habit reminder",
+      body: `${habitInput}`,
     },
     identifier: `${habitInput}`,
     trigger: triggerNotification,
-}).then((id) => {
+  }).then((id) => {
     console.log(id)
-})
+  })
 }
 
-export default { createNotification }
+async function deleteNotification(habitInput) {
+    await Notifications.cancelScheduledNotificationAsync(habitInput)
+    .then(() => {
+        console.log("Deletion successfully made!")
+    })
+}
+
+export default { 
+    createNotification, 
+    deleteNotification 
+}
