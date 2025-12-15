672c728a16e149756592e68012c4e467faec5649
renanmakoto <renanmakoto@outlook.com>
2025-12-15 08:43:26 -0500

    Clean code

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import React from "react"
-import { StatusBar } from 'react-native'
+import React from 'react';
+import { StatusBar } from 'react-native';
 
-import Routes from "./src/routes/Routes"
+import Routes from './src/navigation/Routes';
 
 export default function App() {
   return (
     <>
-      <StatusBar barStyle={"auto"} />
+      <StatusBar barStyle="light-content" backgroundColor="rgba(21, 21, 21, 0.98)" />
       <Routes />
     </>
-  )
+  );
 }
