c554bd1a5b414b97eeacefb8dcd7dbd08c8bf7fb
renanmakoto <renanmakoto@outlook.com>
2025-12-15 08:18:36 -0500

    Clean code

---
+++ src/data/habitsData.js
@@ -0,0 +1,90 @@
+export const mindHabits = [
+  {
+    key: 'Study for 30 minutes',
+    value: 'Study for 30 minutes',
+  },
+  {
+    key: 'Study for 60 minutes',
+    value: 'Study for 60 minutes',
+  },
+  {
+    key: 'Read a book for 15 minutes',
+    value: 'Read a book for 15 minutes',
+  },
+  {
+    key: 'Read a book for 30 minutes',
+    value: 'Read a book for 30 minutes',
+  },
+  {
+    key: 'Work for 30 minutes on a personal project',
+    value: 'Work for 30 minutes on a personal project',
+  },
+  {
+    key: 'Work for 60 minutes on a personal project',
+    value: 'Work for 60 minutes on a personal project',
+  },
+];
+
+export const financeHabits = [
+  {
+    key: 'Save 10% of the monthly income',
+    value: 'Save 10% of the monthly income',
+  },
+  {
+    key: 'Save 20% of the monthly income',
+    value: 'Save 20% of the monthly income',
+  },
+  {
+    key: 'Send 10 freelance project offers',
+    value: 'Send 10 freelance project offers',
+  },
+  {
+    key: 'Send 20 freelance project offers',
+    value: 'Send 20 freelance project offers',
+  },
+];
+
+export const bodyHabits = [
+  {
+    key: 'Have 30 minute cardio',
+    value: 'Have 30 minute cardio',
+  },
+  {
+    key: 'Have an hour cardio',
+    value: 'Have an hour cardio',
+  },
+  {
+    key: 'Drink a litre of water',
+    value: 'Drink a litre of water',
+  },
+  {
+    key: 'Drink two litres of water',
+    value: 'Drink two litres of water',
+  },
+];
+
+export const moodHabits = [
+  {
+    key: 'Have a conversation with a friend',
+    value: 'Have a conversation with a friend',
+  },
+  {
+    key: 'Play video games for 30 minutes',
+    value: 'Play video games for 30 minutes',
+  },
+  {
+    key: 'Watch an episode of your favorite TV show',
+    value: 'Watch an episode of your favorite TV show',
+  },
+  {
+    key: 'Watch fun videos for 20 minutes',
+    value: 'Watch fun videos for 20 minutes',
+  },
+];
+
+export default {
+  dataMind: mindHabits,
+  dataMoney: financeHabits,
+  dataBody: bodyHabits,
+  dataFun: moodHabits,
+};
